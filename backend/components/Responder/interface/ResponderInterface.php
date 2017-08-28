7a9622f6cd8ad1fb800bd5dc4201de6d0bead4d0
gabrielinuz <gabrielinuz@gmail.com>
2017-08-28 08:32:20 -0300

    2017/08/28

---
+++ backend/components/Responder/interface/ResponderInterface.php
@@ -0,0 +1,13 @@
+<?php
+/**
+* Copyright (c) 2013-2016 Gabriel Ferreira <gabrielinuz@gmail.com>. All rights reserved. 
+* This file is part of COMPSET.
+* Released under the MIT license
+* https://opensource.org/licenses/MIT
+**/
+
+interface ResponderInterface
+{
+    public function response( $content );
+}
+?>
