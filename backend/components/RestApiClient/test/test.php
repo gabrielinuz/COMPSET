771acaa36ce64a62845ab2d7400bf851ab3de9f4
gabrielinuz <gabrielinuz@gmail.com>
2017-11-15 11:24:40 -0300

    clean test

--- backend/components/RestApiClient/test/test.php
+++ backend/components/RestApiClient/test/test.php
@@ -14,24 +14,6 @@ $client = ComponentFactory::create('RestApiClient');
 $client->setUrl('https://jsonplaceholder.typicode.com/posts/1');
 $client->setMethod('get');
 
-// $client->setUrl('http://www.ultubrokers.com.ar:21000/ultudemo/interfaces/ultu/KimnInsuranceServices.svc/public/GenerateAutoBudget');
-// $data = array(  "login"=> "cotizador",   
-// 				"password"=> "cotizador",   
-// 				"budget"=> array(    
-// 				"tipoIva"=> "Inscripto",    
-// 				"tipoIIBB"=>"Consumidor Final",    
-// 				"tipoViaDeCobro"=>"TARJETA CREDITO"   ),   
-// 				"item"=>array(    "anio"=> "2015",    
-// 				"codigo"=>"460730",    "codigoPostal"=>"1903",      
-// 				"esCeroKm"=>false,    
-// 				"estadoCivil"=>"Soltero(a)",      
-// 				"fechaNacimiento"=>"/Date(383799600000-0300)/",    
-// 				"provincia"=> "BUENOS AIRES",    
-// 				"localidad"=> "ABASTO",    
-// 				"sexo"=> "Masculino"   )
-// 			);
-// $client->setData($data);
-
 $result = $client->sendRequest();
 
 var_dump($result);
